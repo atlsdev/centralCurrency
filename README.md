1e17c0768c5838f650984884daeba0e83a0b0bfa
Atlas <kontakt@streetracingdenmark.com>
2021-02-27 02:19:49 +0100

    Did a lot of stuff, too much to document.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# centralCurrency
- 
+# Atlas Dev - Central Currency
+This is a project currently In Development.
+It's meant to be a Universal Currency for use in all of Atlas Dev's projects, such as Discord bots with a Point System, or something like that.  
+  
+I would like to have all the balances synced, but I gotta figure out how to sync users up, until then, this project is on hold.
--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 var express = require("express");
+var session = require("express-session");
 var fs = require("fs");
 
 var server = express();
 
 server.use(express.static("public"));
-server.use(require("./routes/users"));
-server.use(require("./routes/currency"));
+server.use(session({ secret: "atlasdevvcurr", resave: false , saveUninitialized: true, cookie: { secure: false } }));
+server.set("views", "./views");
+server.set('view engine', "ejs");
+server.use("/api/users", require("./routes/users"));
+server.use("/api/currency", require("./routes/currency"));
+
+server.get("/", (req, res) => {
+	if(req.session.user) {
+		res.render("index", { user: req.session.user });
+	} else {
+		res.render("index", { user: null });
+	}
+});
+
+server.get("/login", (req, res) => {
+	if(req.session.user) {
+		res.redirect("/");
+	} else {
+		res.render("login");
+	}
+});
+
+server.get("/logout", (req, res) => {
+	if(req.session.user) {
+		req.session.destroy(_ => res.redirect("/"));
+	}
+});
+
+server.get("/generate", (req, res) => {
+	if(req.session.user) {
+		res.render("generate", { user: req.session.user });
+	} else {
+		res.redirect("/login");
+	}
+});
 
 server.listen(99, _ => console.log(`Atlas Currency - Listening on localhost:99`));
